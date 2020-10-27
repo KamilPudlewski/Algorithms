e207d94067bb33b5d52392bf8f06200decc43c5e
Kamil Pudlewski <kamilpudlewski@gmail.com>
2020-10-27 23:45:19 +0100

    Maximum Flow Problem

---
+++ Maximum Flow Problem/Maximum Flow Problem/MaximumFlow.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maximum_Flow_Problem
+{
+    public class MaximumFlow
+    {
+        private int maximumFlow = 0;
+        private GraphMatrix result;
+
+        public MaximumFlow()
+        {
+
+        }
+
+        public void Run(int size, GraphMatrix graph, int source, int destination)
+        {
+            maximumFlow = 0;
+            result = new GraphMatrix(size);
+            int[] ancestors = new int[size];
+            int[] cfp = new int[size];
+            Stack<int> queue = new Stack<int>();
+
+            while(true)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    ancestors[i] = -1;
+                }
+                ancestors[source] = -2;
+                cfp[source] = int.MaxValue;
+                while (queue.Count != 0)
+                {
+                    queue.Pop();
+                }
+                queue.Push(source);
+                bool finishFlag = false;
+                while (queue.Count != 0)
+                {
+                    int vertice = queue.Pop();
+                    for (int i = 0; i < size; i++)
+                    {
+                        int cost = graph.GetValue(vertice, i) - result.GetValue(vertice, i);
+                        if (cost.Equals(0) || !ancestors[i].Equals(-1))
+                            continue;
+
+                        ancestors[i] = vertice;
+                        cfp[i] = Math.Min(cfp[vertice], cost);
+                        if (i == destination)
+                        {
+                            maximumFlow += cfp[destination];
+                            int tmp = destination;
+                            while (!tmp.Equals(source))
+                            {
+                                int w = ancestors[tmp];
+                                result.AddEdge(w, tmp, result.GetValue(w, tmp) + cfp[destination]);
+                                result.AddEdge(tmp, w, result.GetValue(tmp, w) - cfp[destination]);
+                                tmp = w;
+                            }
+                            finishFlag = true;
+                        }
+                        queue.Push(i);
+                    }
+                    if (finishFlag)
+                    {
+                        break;
+                    }
+                }
+                if (!finishFlag)
+                {
+                    break;
+                }
+            }
+        }
+
+        public void PrintResult()
+        {
+            Console.WriteLine("Maximum flow is: " + maximumFlow);
+            Console.WriteLine(result.Print());
+        }
+
+        public void PrintAdvenced()
+        {
+            List<Tuple<int, int>> tmp = new List<Tuple<int, int>>();
+
+            for (int i = 0; i < result.size; i++)
+            {
+                for (int j = 0; j < result.size; j++)
+                {
+                    if (result.graph[i, j] != 0)
+                    {
+                        tmp.Add(new Tuple<int, int>(i, j));
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Maximum Flow Problem/Maximum Flow Problem/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Maximum_Flow_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int size = 7;
+            //GraphMatrix graph = new GraphMatrix(size);
+            //int source = 0;
+            //int destination = size - 1;
+            //graph.AddEdge(source, 1, 9);
+            //graph.AddEdge(source, 4, 9);
+            //graph.AddEdge(1, 2, 7);
+            //graph.AddEdge(1, 3, 3);
+            //graph.AddEdge(2, 3, 4);
+            //graph.AddEdge(2, destination, 6);
+            //graph.AddEdge(3, destination, 9);
+            //graph.AddEdge(3, 5, 2);
+            //graph.AddEdge(4, 3, 3);
+            //graph.AddEdge(4, 5, 6);
+            //graph.AddEdge(5, destination, 8);
+
+            int size = 7;
+            GraphMatrix graph = new GraphMatrix(size);
+            int source = 0;
+            int destination = size - 1;
+            graph.AddEdge(source, 1, 4);
+            graph.AddEdge(source, 2, 3);
+            graph.AddEdge(1, 3, 4);
+            graph.AddEdge(3, destination, 2);
+            graph.AddEdge(2, 4, 6);
+            graph.AddEdge(4, destination, 6);
+            graph.AddEdge(3, 2, 3);
+ 
+
+            MaximumFlow maximumFlow = new MaximumFlow();
+            maximumFlow.Run(size, graph, source, destination);
+            maximumFlow.PrintResult();
+            maximumFlow.PrintAdvenced();
+            Console.ReadKey();
+        }
+    }
+}
