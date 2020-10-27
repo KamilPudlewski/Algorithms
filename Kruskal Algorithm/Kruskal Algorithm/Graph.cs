5dc313019a7d7762e357e4fcc48294679efc87b4
Kamil Pudlewski <kamilpudlewski@gmail.com>
2020-10-27 23:43:33 +0100

    Kruskal Algorithm

---
+++ Kruskal Algorithm/Kruskal Algorithm/Graph.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kruskal_Algorithm
+{
+    public class Graph
+    {
+        public List<Edge> edges = new List<Edge>();
+        public List<int> vertices = new List<int>();
+
+        public Graph()
+        {
+
+        }
+
+        public void AddEdge(Edge edge)
+        {
+            edges.Add(edge);
+            AddVertice(edge.source);
+            AddVertice(edge.destination);
+        }
+
+        public void AddEdge(int source, int destination)
+        {
+            edges.Add(new Edge(source, destination));
+            AddVertice(source);
+            AddVertice(destination);
+        }
+
+        public void AddEdge(int source, int destination, int weight)
+        {
+            edges.Add(new Edge(source, destination, weight));
+            AddVertice(source);
+            AddVertice(destination);
+        }
+
+        public bool EdgeExist(Edge edge)
+        {
+            return edges.Contains(edge);
+        }
+
+        public bool VerticeExist(int vertice)
+        {
+            return vertices.Contains(vertice);
+        }
+
+        public int EdgesCount()
+        {
+            return edges.Count;
+        }
+
+        public int VerticesCount()
+        {
+            return vertices.Count;
+        }
+
+        public void AddVertice(int vertice)
+        {
+            if (!VerticeExist(vertice))
+            {
+                vertices.Add(vertice);
+            }
+        }
+    }
+
+    public class Edge
+    {
+        public int source;
+        public int destination;
+        public int weight;
+
+        public Edge(int source, int destination)
+        {
+            this.source = source;
+            this.destination = destination;
+            weight = 0;
+        }
+
+        public Edge(int source, int destination, int weight)
+        {
+            this.source = source;
+            this.destination = destination;
+            this.weight = weight;
+        }
+    }
+
+    public class Subset
+    {
+        public int parent;
+        public int rank;
+
+        public Subset()
+        {
+
+        }
+
+        public Subset(int parent, int rank)
+        {
+            this.parent = parent;
+            this.rank = rank;
+        }
+    }
+}
---
+++ Kruskal Algorithm/Kruskal Algorithm/Kruskal.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kruskal_Algorithm
+{
+    public class Kruskal
+    {
+        private Graph graph;
+        private List<Edge> result = new List<Edge>();
+
+        public Kruskal(Graph graph)
+        {
+            this.graph = graph;
+            int verticesCount = graph.VerticesCount();
+
+            var sortedEdges = graph.edges.OrderBy(edge => edge.weight).ToList();
+            graph.edges = sortedEdges;
+            
+            Subset[] subsets = new Subset[verticesCount];
+            for (int i = 0; i < subsets.Length; i++)
+            {
+                subsets[i] = new Subset();
+            }
+
+            for (int v = 0; v < verticesCount; v++)
+            {
+                subsets[v].parent = v;
+                subsets[v].rank = 0;
+            }
+
+            int actualEdgeCounter = 0;
+            while (result.Count < verticesCount - 1)
+            {
+                Edge nextEdge = graph.edges[actualEdgeCounter++];
+                int x = Find(subsets, nextEdge.source);
+                int y = Find(subsets, nextEdge.destination);
+
+                if (x != y)
+                {
+                    result.Add(nextEdge);
+                    Union(subsets, x, y);
+                }
+            }
+        }
+
+        private int Find(Subset[] subsets, int i)
+        {
+            if (subsets[i].parent != i)
+            {
+                subsets[i].parent = Find(subsets, subsets[i].parent);
+            }
+            return subsets[i].parent;
+        }   
+
+        private void Union(Subset[] subsets, int x, int y)
+        {
+            int xroot = Find(subsets, x);
+            int yroot = Find(subsets, y);
+
+            // Attach smaller rank tree under root of high rank tree (Union by Rank) 
+            if (subsets[xroot].rank < subsets[yroot].rank)
+            {
+                subsets[xroot].parent = yroot;
+            }
+            else if (subsets[xroot].rank > subsets[yroot].rank)
+            {
+                subsets[yroot].parent = xroot;
+            }
+            else
+            {
+                subsets[yroot].parent = xroot;
+                subsets[xroot].rank++;
+            }
+        }
+
+        public void PrintResult()
+        {
+            int sum = 0;
+            for (int i = 0; i < result.Count; i++)
+            {
+                Console.WriteLine(result[i].source + " -> " + result[i].destination + " == " + result[i].weight);
+                sum += result[i].weight;
+            }
+            Console.WriteLine("Total weights: " + sum);
+        }
+    }
+}
